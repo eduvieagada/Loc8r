aa95ca1b7aabab72f885e11269d52ba4224afdbc
Eduvie <eagada@parkwayprojects.com>
2019-07-15 13:40:22 +0100

    added delete apis for location and review

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+require('./app_api/models/db');
+
 const indexRouter = require('./app_server/routes/index');
-const usersRouter = require('./app_server/routes/users');
-require('./app_server/models/db');
+const apiRouter = require('./app_api/routes/index');
+
 
 const app = express();
 
@@ -21,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_api/models/db.js
+++ app_api/models/db.js
@@ -27,3 +27,5 @@ const gracefulShutdown = (msg, callback) => mongoose.connection.close(() => {
 process.once('SIGUSR2', () => gracefulShutdown('nodemon restart', () => process.kill(process.pid, 'SIGUSR2')));
 process.on('SIGINT', () => gracefulShutdown('app termination', () => process.exit(0)));
 process.on('SIGTERM', () => gracefulShutdown('Heroku app shutdown', () => process.exit(0)));
+
+require('./locations');
